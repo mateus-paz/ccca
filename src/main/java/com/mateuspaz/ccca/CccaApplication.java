8edc2008a18455e2c25dfb54af9b74e3d0e14b88
Mateus Paz <mateuspaz.mp@gmail.com>
2024-06-29 10:57:41 -0300

    initial commit

---
+++ src/main/java/com/mateuspaz/ccca/CccaApplication.java
@@ -0,0 +1,13 @@
+package com.mateuspaz.ccca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateuspaz/ccca/CccaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuspaz.ccca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
